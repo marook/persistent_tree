ca697895a8566193b6c5013a2aaecfd1b5e273c3
Markus Peroebner <markus.peroebner@nttdata.com>
2014-05-28 17:46:35 +0200

    [refactoring] reduced duplication in manipulation test

--- src/test_manipulate_tree.py
+++ src/test_manipulate_tree.py
@@ -9,16 +9,20 @@ class ManipulateTreeTest(unittest.TestCase):
     def test_insert_root_node_in_tree(self):
         node_key = 0
 
-        self.tree.insert_node(create_node(node_key))
+        self.insert_node_with_key(node_key)
 
         self.assertEqual(node_key, self.tree.root_node.key)
 
     def test_insert_two_nodes_in_tree(self):
-        self.tree.insert_node(create_node(0))
-        self.tree.insert_node(create_node(1))
+        self.insert_node_with_key(0)
+        self.insert_node_with_key(1)
 
         self.assertEqual(0, self.tree.root_node.key)
         self.assertEqual(1, self.tree.root_node.left.key)
+
+    def insert_node_with_key(self, key):
+        self.tree.insert_node(create_node(key))
+        
 
 def create_node(key):
     return model.Node(key, '')
