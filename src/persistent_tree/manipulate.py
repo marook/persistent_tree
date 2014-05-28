a3946766fbddec90df34cecfa0d3fe97e607ca10
Markus Peroebner <markus.peroebner@nttdata.com>
2014-05-28 17:44:49 +0200

    implemented tree building

--- src/persistent_tree/manipulate.py
+++ src/persistent_tree/manipulate.py
@@ -16,4 +16,30 @@ class Tree(object):
         self.root_node = None
 
     def insert_node(self, inserted_node):
-        self.root_node = inserted_node
+        if self.root_node is None:
+            self.root_node = inserted_node
+
+            return
+
+        self.root_node = insert_node_in_tree(self.root_node, inserted_node, self.key_comparator)
+
+def insert_node_in_tree(parent_node, inserted_node, key_comparator):
+    cmp_result = key_comparator(parent_node.key, inserted_node.key)
+
+    if cmp_result < 0:
+        if parent_node.left is None:
+            parent_node.left = inserted_node
+        else:
+            parent_node.left = insert_node_in_tree(parent_node.left, inserted_node, key_comparator)
+    elif cmp_result > 0:
+        if parent_node.right is None:
+            parent_node.right = inserted_node
+        else:
+            parent_node.right = insert_node_in_tree(parent_node.right, inserted_node, key_comparator)
+    else:
+        inserted_node.left = parent_node.left
+        inserted_node.right = parent_node.right
+
+        parent_node = inserted_node
+
+    return parent_node
--- src/test_manipulate_tree.py
+++ src/test_manipulate_tree.py
@@ -13,6 +13,13 @@ class ManipulateTreeTest(unittest.TestCase):
 
         self.assertEqual(node_key, self.tree.root_node.key)
 
+    def test_insert_two_nodes_in_tree(self):
+        self.tree.insert_node(create_node(0))
+        self.tree.insert_node(create_node(1))
+
+        self.assertEqual(0, self.tree.root_node.key)
+        self.assertEqual(1, self.tree.root_node.left.key)
+
 def create_node(key):
     return model.Node(key, '')
 
