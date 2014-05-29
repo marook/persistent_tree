67007ab32c3fb507dc753f796b54e3706313a210
Markus Peröbner <markus.peroebner@gmail.com>
2014-05-29 06:43:58 +0200

    added one command to run all tests

--- setup.py
+++ setup.py
@@ -2,7 +2,40 @@
 # -*- coding: utf-8 -*-
 
 from distutils import core
+from glob import glob
+import os
 from os import path
+import sys
+
+class Test(core.Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+
+        import unittest
+
+        sys.path.insert(0, 'src')
+
+        testfiles = [ ]
+        for t in glob(path.join(self._dir, 'src', 'test_*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    [path.splitext(path.basename(t))[0]])
+                )
+
+        tests = unittest.TestLoader().loadTestsFromNames(testfiles)
+        t = unittest.TextTestRunner(verbosity = 1)
+        t.run(tests)
 
 core.setup(
     name = 'persistent_tree',
@@ -16,4 +49,5 @@ core.setup(
     ],
     packages = ['persistent_tree',],
     package_dir = {'': 'src'},
+    cmdclass = {'test': Test},
 )
