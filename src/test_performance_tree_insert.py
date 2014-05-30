70653fa55865ab94450bba128864440d4ea76b0d
Markus Peröbner <markus.peroebner@gmail.com>
2014-05-30 14:03:46 +0200

    implemented failing performance test

--- src/test_performance_tree_insert.py
+++ src/test_performance_tree_insert.py
@@ -6,12 +6,12 @@ import unittest
 class TreeInsertPerformanceTest(unittest.TestCase):
 
     def test(self):
-        sample_sizes = [1000, 500, 4000, 2000]
+        sample_sizes = [100, 1600, 25600]
         samples = self.measure_samples(sample_sizes)
         
-        duration_increases = self.calc_size_duration_increase_ratio(samples)
+        duration_increases = list(self.calc_size_duration_increase_ratio(samples))
 
-        print 'increase ratios: %s' % (', '.join(['%s' % (duration_increase, ) for duration_increase in duration_increases]), )
+        self.assertTrue(duration_increases[1] / duration_increases[0] > 0.5, 'insert ratios: %s' % (', '.join(['%s' % (duration_increase, ) for duration_increase in duration_increases]), ))
         
     def measure_samples(self, sample_sizes):
         samples = {}
@@ -27,17 +27,24 @@ class TreeInsertPerformanceTest(unittest.TestCase):
 
     def measure_build_tree(self, number_of_inserted_nodes):
         tree = manipulate.Tree()
+        nodes = self.build_nodes_list(number_of_inserted_nodes)
 
         start_time = datetime.datetime.now()
 
-        for i in xrange(number_of_inserted_nodes):
-            node = create_node_from_index(i)
-
+        for node in nodes:
             tree.insert_node(node)
 
         end_time = datetime.datetime.now()
 
         return end_time - start_time
+
+    def build_nodes_list(self, number_of_nodes):
+        nodes = []
+
+        for i in xrange(number_of_nodes):
+            nodes.append(create_node_from_index(i))
+
+        return nodes
 
     def calc_size_duration_increase_ratio(self, samples):
         sorted_sample_sizes = sorted(samples.iterkeys())
@@ -55,4 +62,27 @@ class TreeInsertPerformanceTest(unittest.TestCase):
         return float(sample_size) / sample_duration.total_seconds()
 
 def create_node_from_index(i):
+    i = switch_bits(i, 0, 5)
+    i = switch_bits(i, 1, 7)
+    i = switch_bits(i, 2, 4)
+    i = switch_bits(i, 3, 8)
+    i = switch_bits(i, 6, 9)
+    i = switch_bits(i, 10, 11)
+
     return model.Node(i, None)
+
+def switch_bits(i, b0, b1):
+    v0 = (i & (1 << b0) >> b0)
+    v1 = (i & (1 << b1) >> b1)
+
+    if v0 == 0:
+        i &= ~(1 << b1)
+    else:
+        i |= 1 << b1
+
+    if v1 == 0:
+        i &= ~(1 << b0)
+    else:
+        i |= 1 << b0
+
+    return i
