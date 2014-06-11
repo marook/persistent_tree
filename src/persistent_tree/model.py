a301fe47c434b946255e8aa71210f927acbd4964
Markus Peroebner <markus.peroebner@nttdata.com>
2014-06-11 17:52:45 +0200

    using __slots__ in Node class now to reduce memory footprint

--- src/persistent_tree/model.py
+++ src/persistent_tree/model.py
@@ -1,6 +1,8 @@
 
 class Node(object):
 
+    __slots__ = ['key', 'data', 'parent', '_left', '_right', 'height']
+
     def __init__(self, key, data):
         self.key = key
         self.data = data
